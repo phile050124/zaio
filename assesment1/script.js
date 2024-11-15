8b217f3d80d60e71cb0ea8bdff428d7f644738d0
phile050124 <sefodiphile0501@gmail.com>
2024-11-15 02:42:51 +0200

    assesment added

---
+++ assesment1/script.js
@@ -0,0 +1,21 @@
+/* ========================================== 
+scrollTop() >= 300
+Should be equal the the height of the header
+========================================== */
+
+$(window).scroll(function(){
+    if ($(window).scrollTop() >= 300) {
+        $('nav').addClass('fixed-header');
+        $('nav div').addClass('visible-title');
+    }
+    else {
+        $('nav').removeClass('fixed-header');
+        $('nav div').removeClass('visible-title');
+    }
+});
+
+$(".option").click(function(){
+    $(".option").removeClass("active");
+    $(this).addClass("active");
+    
+ });
