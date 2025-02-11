f824773c18e302c65346a3569def2ae1c204ece6
phile050124 <148635989+phile050124@users.noreply.github.com>
2025-02-11 18:49:54 +0200

    Update userRoutes.js

--- BookVerse/ routes/userRoutes.js
+++ BookVerse/ routes/userRoutes.js
@@ -1,5 +1,12 @@
 const express = require('express');
-const { createUser, getUserById, updateUser, deleteUser } = require('../controllers/userController');
+const {
+  createUser,
+  getUserById,
+  updateUser,
+  deleteUser,
+  addToWishlist,
+  removeFromWishlist,
+} = require('../controllers/userController');
 
 const router = express.Router();
 
@@ -8,5 +15,8 @@ router.get('/:id', getUserById);
 router.put('/:id', updateUser);
 router.delete('/:id', deleteUser);
 
-module.exports = router;
+// Wishlist management
+router.post('/:id/wishlist', addToWishlist);
+router.delete('/:id/wishlist/:bookId', removeFromWishlist);
 
+module.exports = router;
