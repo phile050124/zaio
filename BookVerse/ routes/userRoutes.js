36468c912ff188c8e5525925df559698bde64430
phile050124 <148635989+phile050124@users.noreply.github.com>
2025-02-11 18:40:57 +0200

    Update userRoutes.js

--- BookVerse/ routes/userRoutes.js
+++ BookVerse/ routes/userRoutes.js
@@ -1,1 +1,12 @@
+const express = require('express');
+const { createUser, getUserById, updateUser, deleteUser } = require('../controllers/userController');
+
+const router = express.Router();
+
+router.post('/', createUser);
+router.get('/:id', getUserById);
+router.put('/:id', updateUser);
+router.delete('/:id', deleteUser);
+
+module.exports = router;
 
