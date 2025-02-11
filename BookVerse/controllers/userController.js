a97c671522f8e7bb35086f2ff78291fe057ae37f
phile050124 <148635989+phile050124@users.noreply.github.com>
2025-02-11 18:51:37 +0200

    Update userController.js

--- BookVerse/controllers/userController.js
+++ BookVerse/controllers/userController.js
@@ -46,3 +46,55 @@ const deleteUser = async (req, res) => {
 
 module.exports = { createUser, getUserById, updateUser, deleteUser };
 
+
+const User = require('../models/User');
+const Book = require('../models/Book');
+
+// Add a book to the user's wishlist
+const addToWishlist = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) return res.status(404).json({ message: 'User not found' });
+
+    const book = await Book.findById(req.body.bookId);
+    if (!book) return res.status(404).json({ message: 'Book not found' });
+
+    if (!user.wishlist.includes(book._id)) {
+      user.wishlist.push(book._id);
+      await user.save();
+      return res.status(200).json({ message: 'Book added to wishlist', wishlist: user.wishlist });
+    }
+
+    res.status(400).json({ message: 'Book is already in wishlist' });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// Remove a book from the user's wishlist
+const removeFromWishlist = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) return res.status(404).json({ message: 'User not found' });
+
+    const bookIndex = user.wishlist.indexOf(req.params.bookId);
+    if (bookIndex > -1) {
+      user.wishlist.splice(bookIndex, 1);
+      await user.save();
+      return res.status(200).json({ message: 'Book removed from wishlist', wishlist: user.wishlist });
+    }
+
+    res.status(400).json({ message: 'Book not found in wishlist' });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = {
+  createUser,
+  getUserById,
+  updateUser,
+  deleteUser,
+  addToWishlist,
+  removeFromWishlist,
+};
